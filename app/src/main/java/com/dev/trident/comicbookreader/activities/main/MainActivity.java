93f59e4abe62b1cfada34eb02b6289b387cf3dea
trident2710 <andriydychka@gmail.com>
2017-04-13 18:51:03 +0200

    Added icons, layout changes and code impl for navigation drawer

--- app/src/main/java/com/dev/trident/comicbookreader/activities/main/MainActivity.java
+++ app/src/main/java/com/dev/trident/comicbookreader/activities/main/MainActivity.java
@@ -1,14 +1,29 @@
 package com.dev.trident.comicbookreader.activities.main;
 
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.dev.trident.comicbookreader.R;
 import com.dev.trident.comicbookreader.activities.main.presenter.MainPresenter;
 import com.dev.trident.comicbookreader.activities.main.presenter.MainPresenterImpl;
 import com.dev.trident.comicbookreader.activities.main.view.MainView;
 
-public class MainActivity extends AppCompatActivity implements MainView{
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements MainView, NavigationView.OnNavigationItemSelectedListener {
+
+    @Bind(R.id.clMainActivity)
+    CoordinatorLayout coordinatorLayout;
+    @Bind(R.id.dlMainActivity)
+    DrawerLayout drawerLayout;
+    @Bind(R.id.nvMainActivity)
+    NavigationView navigationView;
 
     private MainPresenter mainPresenter;
     /**
@@ -16,7 +31,17 @@ public class MainActivity extends AppCompatActivity implements MainView{
      * Should be called in onCreate()
      */
     void init(){
+        ButterKnife.bind(this);
         mainPresenter = new MainPresenterImpl(this);
+
+        Toolbar mActionBarToolbar = (Toolbar) findViewById(R.id.tbMainActivity);
+        setSupportActionBar(mActionBarToolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu_left);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+        }
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
@@ -24,5 +49,10 @@ public class MainActivity extends AppCompatActivity implements MainView{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        return false;
     }
 }
